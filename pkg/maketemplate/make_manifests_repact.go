ebd8160020863e9ff7507756be8e12c169868ff2
kuku.choi <kakao_ent@kukuchoi.local>
2025-02-18 19:07:09 +0900

    makeManifests.go 파일 리팩토링 - 생성자를 통한 코드 재활용

---
+++ pkg/maketemplate/make_manifests_repact.go
@@ -0,0 +1,66 @@
+package maketemplate
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	_ "io/fs"
+	"os"
+
+	_ "gopkg.in/yaml.v2"
+)
+
+type TemplateData struct {
+	Values map[string]interface{}
+}
+
+type ManifestConstructor struct {
+	InputPath    string
+	OutputDir    string
+	templateData *TemplateData
+}
+
+func NewManifestConstructor(inputFile, outputDir string, templateData *TemplateData) *ManifestConstructor {
+	return &ManifestConstructor{
+		InputPath:    inputFile,
+		OutputDir:    outputDir,
+		templateData: templateData,
+	}
+}
+
+func (mc *ManifestConstructor) RenderManifest() error {
+	tmplData, err := os.ReadFile(mc.InputPath + manifest)
+	if err != nil {
+		fmt.Println("Error reading template file:", err)
+		return err
+	}
+
+	tmpl, err := template.New("manifest").Parse(string(tmplData))
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return err
+	}
+
+	var rendered bytes.Buffer
+	err = tmpl.Execute(&rendered, t)
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		return err
+	}
+
+	_, err = os.Stat(mc.OutputDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			os.Mkdir("base", 0744)
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+	err = os.WriteFile("base/"+mc.OutputDir, rendered.Bytes(), 0644)
+	if err != nil {
+		fmt.Println("Error Create Yaml:", err)
+	}
+
+	return nil
+}
